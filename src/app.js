ffaa98a957b32185d090c5060416bc6131bddad6
Dawn <dawn@DawndeMacBook-Pro.local>
2020-10-06 21:24:45 +0800

    feat: 增加session配置

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ const json = require('koa-json');
 const onerror = require('koa-onerror');
 const bodyparser = require('koa-bodyparser');
 const logger = require('koa-logger');
+const session = require('koa-generic-session');
+const redisStore = require('koa-redis');
+
+const { REDIS_CONF } = require('./conf/db');
 
 const index = require('./routes');
 const users = require('./routes/users');
@@ -24,13 +28,29 @@ app.use(views(__dirname + '/views', {
   extension: 'ejs'
 }));
 
+// session 配置
+app.keys = ['Udsd_768*#f'];
+app.use(session({
+  key: 'weibo.sid', // cookie name 默认是 koa.sid
+  prefix: 'weibo:sess:', // redis key 的前缀，默认是 koa:sess
+  cookie: {
+    path: '/',
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000 // ms
+  },
+  // ttl: 24 * 60 * 60 * 1000,
+  store: redisStore({
+    all: `${REDIS_CONF.host}:${REDIS_CONF.port}`
+  })
+}))
+
 // logger
-app.use(async (ctx, next) => {
-  const start = new Date();
-  await next();
-  const ms = new Date() - start;
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
-});
+// app.use(async (ctx, next) => {
+//   const start = new Date();
+//   await next();
+//   const ms = new Date() - start;
+//   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+// });
 
 // routes
 app.use(index.routes(), index.allowedMethods());
